54937c33d102bf3a57a2a4f1689189395a79443a
silvia <silvia.ms999@gmail.com>
2025-03-02 01:18:47 +0100

    verión final

--- src/main/java/hotel/aplicacion/Utilidades.java
+++ src/main/java/hotel/aplicacion/Utilidades.java
@@ -1,8 +1,9 @@
 /*
+ *
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package hotel.aplicacion;
+package hotel.utilidades;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,7 +12,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Silvia
+ * @author Antonio
  */
 public class Utilidades {
   
